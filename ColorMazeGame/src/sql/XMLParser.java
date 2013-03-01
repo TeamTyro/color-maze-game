ec0d413a034577adf95ba6ec01e73c9170dd9478
Jack Ketcham <kcajketcham@gmail.com>
2013-03-01 08:54:24 -0700

    Add broken proprietary data sending.

--- ColorMazeGame/src/sql/XMLParser.java
+++ ColorMazeGame/src/sql/XMLParser.java
@@ -11,51 +11,31 @@ public class XMLParser {
 	}
 	
 	public XMLParser(InfoPackage pack) {
-		String x_0_s = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-		String x_1_s = "<data>";
-		String x_1_e = "</data>";
-		String x_2_s = "<stime>";
-		String x_2_e = "</stime>";
-		String x_3_s = "<etime>";
-		String x_3_e = "</etime>";
-		String x_4_s = "<moves>";
-		String x_4_e = "</moves>";
+		xml = xml.concat(String.valueOf(pack.getDate(1)));
+		xml = xml.concat("|");
+		xml = xml.concat(String.valueOf(pack.getDate(2)));
+		xml = xml.concat("|");
+		int [] actions = pack.getActions();
+		int index = 0;
+		while(actions[index] != 0 && index < 500) {
+			switch(actions[index]) {
+			case Constants.DIR_LEFT:
+				xml = xml.concat("l");
+				break;
+			case Constants.DIR_RIGHT:
+				xml = xml.concat("r");
+				break;
+			case Constants.DIR_UP:
+				xml = xml.concat("u");
+				break;
+			case Constants.DIR_DOWN:
+				xml = xml.concat("d");
+				break;
+			}
+			index++;
+		}
 		
-		xml = "";
-		xml = xml.concat(x_0_s);
-		xml = xml.concat(x_1_s);
-		
-			xml = xml.concat(x_2_s);
-				xml = xml.concat(String.valueOf(pack.getDate(1)));
-			xml = xml.concat(x_2_e);
-			
-			xml = xml.concat(x_3_s);
-				xml = xml.concat(String.valueOf(pack.getDate(2)));
-			xml = xml.concat(x_3_e);
-			
-			xml = xml.concat(x_4_s);
-				int [] actions = pack.getActions();
-				int index = 0;
-				while(actions[index] != 0 && index < 500) {
-					switch(actions[index]) {
-					case Constants.DIR_LEFT:
-						xml = xml.concat("l");
-						break;
-					case Constants.DIR_RIGHT:
-						xml = xml.concat("r");
-						break;
-					case Constants.DIR_UP:
-						xml = xml.concat("u");
-						break;
-					case Constants.DIR_DOWN:
-						xml = xml.concat("d");
-						break;
-					}
-					index++;
-				}
-				
-			xml = xml.concat(x_4_e);
-		xml = xml.concat(x_1_e);
+		xml = xml.concat("|");
 	}
 	
 	public String getXML() {
